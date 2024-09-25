cf3c64eac41ce5d5bd3b0fac1336665478ec5c2d
Iara <iara.vitabarhuber@gmail.com>
2024-09-25 08:31:59 -0300

    de nuevo

--- trello_ejs/src/components/Board.jsx
+++ trello_ejs/src/components/Board.jsx
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import List from './List';
-import NewListForm from './NewListForm';
+import React from "react";
+import Column from './Column';
 
-const Board = () =>{
-    const [lists, setLists] = useState([]);
-
-    useEffect (()=> {
-        const fetchData = async () => {
-            const boardData = [
-                {id: 1, title: "To Do", cards:[{id:1,content:"Task 1" }, { id: 2, content: "Task 2" }] },
-                { id: 2, title: "In Progress", cards: [{ id: 3, content: "Task 3" }] }
-            ];
-            setLists(boardData);
-        };
-        fetchData();
-    }, []);
-
-    const addList = (title)=>{
-        const newList = { id: lists.length + 1, title, cards: [] };
-        setLists([...lists, newList]);
-    };
-    return(
+function Board({coluns, addTask}) {
+    return (
         <div className="board">
-            <h1> Tablero</h1>
-            <div className="list container">
-                {lists.map(list=>(
-                    <List key = {list.id} list ={list} />
-                ))}
-                <NewListForm addList={addList}/>
-            </div>
+            {columns.map(column =>(
+                <Column key={column.id} column ={column} addTask={addTask}/>
+            ))}
         </div>
     );
-};
-
+}
 export default Board;
